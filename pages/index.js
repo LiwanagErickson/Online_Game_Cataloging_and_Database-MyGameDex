e11b02618d1799d15cb32c4daa18a64614598c18
Erickson <liwanagerickson20@gmail.com>
2022-05-03 12:26:54 +0800

    Made changes

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,8 @@ import {Button, Box, Typography } from '@mui/material/';
 export default function index() {
  return (
   <Box component="span" sx={{border: '5px dashed red', display: 'flex'}}>
-      <Typography variant="h1" component="h2">
-        Hello
+      <Typography variant="h1" component="h1">
+        Hello World
       </Typography>
     </Box>
  );
