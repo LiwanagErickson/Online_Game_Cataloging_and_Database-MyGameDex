3e954de242ce73db7ffd2cdbb4f8b81894102699
Erickson <liwanagerickson20@gmail.com>
2022-05-16 09:51:40 +0800

    Added router

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,49 @@
 import * as React from 'react';
 import {Button, Box, Typography, AppBar, Toolbar, IconButton} from '@mui/material/';
 import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
+import { useRouter } from "next/router";
 
-const pages = ['Genres', 'Rankings', 'About US'];
-const settings = ['Profile', 'Account Settings', 'Logout'];
+const AccountType = "Admin";
 
 export default function index() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const router = useRouter();
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
 
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
 
+  const gotoRankings = ()=>{
+    router.push("/rankings")
+  }
+  const gotoGenres = ()=>{
+    router.push("/genres")
+  }
+  const gotoAboutUS = ()=>{
+    router.push("/aboutus")
+  }
+  const gotoSettings = ()=>{
+    router.push("/settings")
+  }
+  const gotoProfile = ()=>{
+    router.push("/profile")
+  }
+
  return (
   <><AppBar position="static">
      <Container maxWidth="xl">
        <Toolbar disableGutters>
-       <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+       
          <Typography
            variant="h6"
            noWrap
@@ -52,44 +61,6 @@ export default function index() {
          >
            myGameDex
          </Typography>
-
-         <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-           <IconButton
-             size="large"
-             aria-label="account of current user"
-             aria-controls="menu-appbar"
-             aria-haspopup="true"
-             onClick={handleOpenNavMenu}
-             color="inherit"
-           >
-             <MenuIcon />
-           </IconButton>
-           <Menu
-             id="menu-appbar"
-             anchorEl={anchorElNav}
-             anchorOrigin={{
-               vertical: 'bottom',
-               horizontal: 'left',
-             }}
-             keepMounted
-             transformOrigin={{
-               vertical: 'top',
-               horizontal: 'left',
-             }}
-             open={Boolean(anchorElNav)}
-             onClose={handleCloseNavMenu}
-             sx={{
-               display: { xs: 'block', md: 'none' },
-             }}
-           >
-             {pages.map((page) => (
-               <MenuItem key={page} onClick={handleCloseNavMenu}>
-                 <Typography textAlign="center">{page}</Typography>
-               </MenuItem>
-             ))}
-           </Menu>
-         </Box>
-         <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
          <Typography
            variant="h5"
            noWrap
@@ -109,15 +80,27 @@ export default function index() {
            LOGO
          </Typography>
          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-           {pages.map((page) => (
-             <Button
-               key={page}
-               onClick={handleCloseNavMenu}
+            <Button
+               key={'genres'}
+               onClick={gotoGenres}
                sx={{ my: 2, color: 'white', display: 'block' }}
              >
-               {page}
-             </Button>
-           ))}
+               Genres
+            </Button>
+            <Button
+               key={'rankings'}
+               onClick={gotoRankings}
+               sx={{ my: 2, color: 'white', display: 'block' }}
+             >
+               Rankings
+            </Button>
+            <Button
+               key={'aboutus'}
+               onClick={gotoAboutUS}
+               sx={{ my: 2, color: 'white', display: 'block' }}
+             >
+               About US
+            </Button>
          </Box>
 
          <Box sx={{ flexGrow: 0 }}>
@@ -141,12 +124,16 @@ export default function index() {
              }}
              open={Boolean(anchorElUser)}
              onClose={handleCloseUserMenu}
-           >
-             {settings.map((setting) => (
-               <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                 <Typography textAlign="center">{setting}</Typography>
-               </MenuItem>
-             ))}
+           > 
+              <MenuItem key={'Profile'} onClick={gotoProfile}>
+                 <Typography textAlign="center">{'Profile'}</Typography>
+              </MenuItem>
+              <MenuItem key={'Account Settings'} onClick={gotoSettings}>
+                 <Typography textAlign="center">{'Account Settings'}</Typography>
+              </MenuItem>
+              <MenuItem key={'Logout'} onClick={handleCloseUserMenu}>
+                 <Typography textAlign="center">{'Logout'}</Typography>
+              </MenuItem>
            </Menu>
          </Box>
        </Toolbar>
