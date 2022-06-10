1ccdc53bf28edd134a180b7d4eb2250e585dce09
unknown <talastas444@gmail.com>
2022-06-10 20:04:05 +0800

    final

--- src/pages/gamelist.js
+++ src/pages/gamelist.js
@@ -1,16 +1,29 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import {Box, Button} from '@mui/material';
+import {Box, Button, Grid} from '@mui/material';
 import { GameData } from "../components/json/GameData";
 import UserLayout from '../components/UserLayout';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
 
 export default function gamelist() {
   return (
-  <Box component="div" sx={{ width: "100vw", height: "60vh", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
+<React.Fragment>
+    <CssBaseline />
+
+
+    <Container maxWidth="lg">
+    <Grid container>
+  <Box component="div" sx={{ width: "100vw", height: "60vh", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column",my: 6, ml: 5 }}>
    {GameData.gameData.map((gameData) => (
                   <Button href={'/gameview?id=' + gameData.id}>{gameData.name}</Button>  
               ))}
      </Box>
+    </Grid>
+     </Container>
+  </React.Fragment>
+
+
   );
   
 }
